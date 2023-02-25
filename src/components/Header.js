f0755442b23685322f261dd10d7adc773f537aa3
Ana Aparicio <aaparici@student.42madrid.com>
2023-02-25 13:22:36 +0100

    props 01

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,18 +3,19 @@ import PropTypes from 'prop-types'
 const name = 'Klitchka';
 const x = true;
 
+
 export const Header = ({ title }) => {
   return (
     <header>
-      <h1>{title}</h1>
+        <h1>{title}</h1>
         <h1>Hello {name}</h1>
         <h2>Are you happy? {x ? 'Sí' : 'No'}</h2>
     </header>
   )
+  
 }
 
 Header.defaultProps = {
   title: "holi",
 }
 
-export default Header
