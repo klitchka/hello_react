760ce9342272694a9428f35e5cce326edc0b7c19
Ana Aparicio <aaparici@student.42madrid.com>
2023-02-26 17:31:53 +0100

    english

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@
 /*   By: aaparici <aaparici@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/24 23:48:28 by aaparici          #+#    #+#             */
-/*   Updated: 2023/02/26 17:14:40 by aaparici         ###   ########.fr       */
+/*   Updated: 2023/02/26 17:30:37 by aaparici         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,7 @@ function App() {
   
   return (
     <div className="container">
-      <Header title="Thanks for passing by"></Header>
+      <Header title="Thanks for passing by."></Header>
     </div>
    
   );
--- src/components/Header.js
+++ src/components/Header.js
@@ -6,7 +6,7 @@
 /*   By: aaparici <aaparici@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/26 14:16:51 by aaparici          #+#    #+#             */
-/*   Updated: 2023/02/26 17:21:12 by aaparici         ###   ########.fr       */
+/*   Updated: 2023/02/26 17:31:39 by aaparici         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,7 +20,7 @@ export const Header = ({ title }) => {
   return (
     <header>
         <h1 style={headerstyle}>Hello {name}</h1>
-        <h2 style={headerstyle}>Are you happy? {x ? 'Sí' : 'No'}</h2>
+        <h2 style={headerstyle}>Are you happy? {x ? 'Yes' : 'No'}</h2>
         <br></br>
         <br></br>
         <br></br>
