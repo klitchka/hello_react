8649cc4a3fa07ff7e64a3821f163cf7bd7d292e2
Ana Aparicio <aaparici@student.42madrid.com>
2023-02-26 17:51:41 +0100

    how are u

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,21 +6,25 @@
 /*   By: aaparici <aaparici@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/26 14:16:51 by aaparici          #+#    #+#             */
-/*   Updated: 2023/02/26 17:38:39 by aaparici         ###   ########.fr       */
+/*   Updated: 2023/02/26 17:51:09 by aaparici         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 import PropTypes from 'prop-types'
 
 const name = 'Klitchka';
-const x = true;
+const happy = true;
+const love = true;
+const healthy = false;
 
 
 export const Header = ({ title }) => {
   return (
     <header>
         <h1 style={headerstyle}>Hello {name}</h1>
-        <h2 style={headerstyle}>Are you happy? {x ? 'Yes' : 'No'}</h2>
+        <h2 style={headerstyle}>Are you happy? {happy ? 'Yes' : 'No'}</h2>
+        <h2 style={headerstyle}>Are you in love? {love ? 'Yes' : 'No'}</h2>
+        <h2 style={headerstyle}>Are you healthy? {healthy ? 'Yes' : 'No'}</h2>
         <br></br>
         <br></br>
         <br></br>
