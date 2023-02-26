05df9574aa64bfa461176119c9dec9acd25e596c
Ana Aparicio <aaparici@student.42madrid.com>
2023-02-26 17:16:13 +0100

    title

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,7 +6,7 @@
 /*   By: aaparici <aaparici@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/26 14:16:51 by aaparici          #+#    #+#             */
-/*   Updated: 2023/02/26 17:01:41 by aaparici         ###   ########.fr       */
+/*   Updated: 2023/02/26 17:15:58 by aaparici         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,9 +19,9 @@ const x = true;
 export const Header = ({ title }) => {
   return (
     <header>
-        <h1>{title}</h1>
         <h1 style={headerstyle}>Hello {name}</h1>
         <h2 style={headerstyle}>Are you happy? {x ? 'Sí' : 'No'}</h2>
+        <p style={headerstyle}>{title}</p>
     </header>
   )
   
