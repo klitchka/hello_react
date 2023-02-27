221fb8d4c063392340546bba2ab575327546b775
Ana Aparicio <aaparici@student.42madrid.com>
2023-02-27 21:13:28 +0100

    <br></br>

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,7 +6,7 @@
 /*   By: aaparici <aaparici@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/26 14:16:51 by aaparici          #+#    #+#             */
-/*   Updated: 2023/02/27 20:54:33 by aaparici         ###   ########.fr       */
+/*   Updated: 2023/02/27 21:12:30 by aaparici         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,15 +21,13 @@ const healthy = false;
 export const Header = ({ title }) => {
   return (
     <header>
+      <p>
         <h1 style={headerstyle}>Hello {name}</h1>
         <h2 style={headerstyle}>Are you happy? {happy ? 'Yes' : 'No'}</h2>
         <h2 style={headerstyle}>Are you in love? {love ? 'Yes' : 'No'}</h2>
         <h2 style={headerstyle}>Are you healthy? {healthy ? 'Yes' : 'No'}</h2>
-        <br></br>
-        <br></br>
-        <br></br>
-        <br></br>
-        <br></br>
+      </p>
+      <br></br><br></br><br></br>
         <p>{title}</p>
         <button>Give love</button>
     </header>
