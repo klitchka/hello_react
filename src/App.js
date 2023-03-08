209996c538a85842a3b3dfc6536803e125a48198
Ana Aparicio <aaparici@student.42madrid.com>
2023-03-09 00:22:25 +0100

    boton manda señal

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import {MainSquare } from "./components/Main";
-//import React from "react";
+import { MainSquare } from "./components/Main";
+import React from "react";
 import 'firebase/auth';
 <meta name="viewport" content="width=device-width, initial-scale=1.0"></meta>
 
