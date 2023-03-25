e4cd3397e67df404ac800277f152d2f9ae316f7e
Ana Aparicio <aaparici@student.42madrid.com>
2023-03-25 19:30:51 +0100

    almacena valor

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,31 +1,25 @@
-import { useState } from 'react';
 import LikeButton from './Button'
-import Status from './StatusButton.js';
+import StatusButton from './StatusButton.js';
 
 const name = 'Klitchka';
 
 export const MainSquare = ({ title }) => {
-  const [happy, setHappy] = useState(true);
-  const [love, setLove] = useState(true);
-  const [healthy, setHealthy] = useState(false);
 
   return (
     <header>
       <p>
         <h1 style={mainstyle}>Hello {name}</h1>
         <h1 style={questions}>Are you happy?
-        <Status>
-        {happy ? 'Yes' : 'No'}</Status></h1>
-
+        <StatusButton name="happy" />
+        </h1>
 
         <h1 style={questions}>Are you in love? 
-        <Status>
-        {love ? 'Yes' : 'No'}</Status></h1>
-
+        <StatusButton name="love" />
+        </h1>
 
         <h1 style={questions}>Are you healthy? 
-        <Status>
-        {healthy ? 'Yes' : 'No'}</Status></h1>
+        <StatusButton name="healthy" />
+        </h1>
 
       </p>
       <br></br><br></br><br></br>
--- src/components/StatusButton.js
+++ src/components/StatusButton.js
@@ -1,14 +1,14 @@
 import React, { useState } from 'react';
 
-function Status() {
+function StatusButton(props) {
   const [isLiked, setIsLiked] = useState(
-    localStorage.getItem('isLiked') === 'true' ? true : false
+    localStorage.getItem(props.name) === 'true' ? true : false
   );
 
   function handleButtonClick() {
     const newIsLiked = !isLiked;
     setIsLiked(newIsLiked);
-    localStorage.setItem('isLiked', newIsLiked);
+    localStorage.setItem(props.name, newIsLiked);
     console.log(newIsLiked ? 'yes' : 'no');
   }
 
@@ -19,4 +19,4 @@ function Status() {
   );
 }
 
-export default Status;
+export default StatusButton;
