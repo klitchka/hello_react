b6e0b894ee97f0c92988e417613d72b368de3973
Ana Aparicio <aaparici@student.42madrid.com>
2023-03-26 01:53:46 +0100

    ip

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,31 +1,53 @@
-import LikeButton from './Button'
+import { useState } from 'react';
+import LikeButton from './Button';
 import StatusButton from './StatusButton.js';
 
 const name = 'Klitchka';
+const allowedIP = '127.0.0.1'; // Dirección IP permitida para realizar cambios
 
 export const MainSquare = ({ title }) => {
 
+  const [status, setStatus] = useState({
+    happy: false,
+    love: false,
+    healthy: false
+  });
+
+  const handleStatusChange = (statusName) => {
+    const clientIP = window?.RTCPeerConnection?.getConfiguration()?.iceServers[0]?.urls; // Obtiene la dirección IP del cliente
+    if (clientIP === allowedIP) {
+      setStatus(prevStatus => ({
+        ...prevStatus,
+        [statusName]: !prevStatus[statusName]
+      }));
+    } else {
+      console.log('No se permite realizar cambios desde esta dirección IP');
+    }
+  };
+
   return (
     <header>
+      <div class="animated-background">
       <p>
         <h1 style={mainstyle}>Hello {name}</h1>
         <h1 style={questions}>Are you happy?&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-        <StatusButton name="happy" />
+        <StatusButton name="happy" onChange={() => handleStatusChange('happy')} disabled={!status.happy} />
         </h1>
 
         <h1 style={questions}>Are you in love? &nbsp;&nbsp;&nbsp;
-        <StatusButton name="love" />
+        <StatusButton name="love" onChange={() => handleStatusChange('love')} disabled={!status.love} />
         </h1>
 
         <h1 style={questions}>Are you healthy? &nbsp;&nbsp;
-        <StatusButton name="healthy" />
+        <StatusButton name="healthy" onChange={() => handleStatusChange('healthy')} disabled={!status.healthy} />
         </h1>
 
       </p>
       <br></br><br></br><br></br>
         <p>{title}</p>
         <br></br>
-        <LikeButton text='Give Love'></LikeButton>        
+        <LikeButton text='Give Love'></LikeButton>      
+        </div>  
     </header>
   )
   
