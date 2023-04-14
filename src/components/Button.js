7b0ef62b90bd2acd63cb1f046faf5a236698610e
Ana Aparicio <aaparici@student.42madrid.com>
2023-04-14 22:26:37 +0200

    experimento

--- src/components/Button.js
+++ src/components/Button.js
@@ -8,13 +8,13 @@ import {
 } from 'firebase/firestore';
 
   const firebaseConfig = {
-    apiKey: process.env.REACT_APP_API_KEY,
-    authDomain: process.env.REACT_APP_AUTH_DOMAIN,
-    projectId: process.env.REACT_APP_PROJECT_ID,
-    storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
-    messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
-    appId: process.env.REACT_APP_APP_ID,
-    measurementId: process.env.REACT_APP_MEASUREMENT_ID
+    apiKey: "AIzaSyB-SRCyw_fzQm6QzOCcka-pyP9qCwKDJl0",
+    authDomain: "hello-klitchka.firebaseapp.com",
+    projectId: "hello-klitchka",
+    storageBucket: "hello-klitchka.appspot.com",
+    messagingSenderId: "37045171502",
+    appId: "1:37045171502:web:e9cfea68e63a257258b6bf",
+    measurementId: "G-KBSY38VQJ5"
   };
 
 const app = initializeApp(firebaseConfig);
