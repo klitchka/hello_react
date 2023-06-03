72b28a4d4529ece05ea6fa438262a5c3146c33d1
Ana Aparicio <aaparici@student.42madrid.com>
2023-06-03 15:20:10 +0200

    likecount2

--- src/components/MainSquare.js
+++ src/components/MainSquare.js
@@ -2,21 +2,23 @@ import React, { useState, useEffect } from 'react';
 import { getFirestore, doc, onSnapshot, updateDoc } from 'firebase/firestore';
 
 const MainSquare = ({ selectedDB }) => {
-  const [title, setTitle] = useState("");
+  const [title, setTitle] = useState('');
   const [happy, setHappy] = useState(false);
   const [healthy, setHealthy] = useState(false);
   const [love, setLove] = useState(false);
+  const [likeCount, setLikeCount] = useState(0);
 
   useEffect(() => {
     const db = getFirestore();
 
-    const docRef = doc(db, "Friends", selectedDB);
+    const docRef = doc(db, 'Friends', selectedDB);
     const unsubscribe = onSnapshot(docRef, (doc) => {
       const data = doc.data();
       setTitle(`Hello, ${selectedDB}`);
       setHappy(data.happy);
       setHealthy(data.healthy);
       setLove(data.love);
+      setLikeCount(data.likeCount || 0);
     });
 
     return () => {
@@ -26,10 +28,19 @@ const MainSquare = ({ selectedDB }) => {
 
   const handleButtonToggle = async (field) => {
     const db = getFirestore();
-    const docRef = doc(db, "Friends", selectedDB);
+    const docRef = doc(db, 'Friends', selectedDB);
 
     await updateDoc(docRef, {
       [field]: !eval(field),
+    });
+  };
+
+  const handleLike = async () => {
+    const db = getFirestore();
+    const docRef = doc(db, 'Friends', selectedDB);
+
+    await updateDoc(docRef, {
+      likeCount: likeCount + 1,
     });
   };
 
@@ -37,23 +48,24 @@ const MainSquare = ({ selectedDB }) => {
     <div>
       <h1>{title}</h1>
       <p>
-        Are you happy? 
+        Happy:
         <button onClick={() => handleButtonToggle('happy')}>
           {happy ? 'Yes' : 'No'}
         </button>
       </p>
       <p>
-        Are you healthy? 
+        Healthy:
         <button onClick={() => handleButtonToggle('healthy')}>
           {healthy ? 'Yes' : 'No'}
         </button>
       </p>
       <p>
-        Are you in love?
+        Love:
         <button onClick={() => handleButtonToggle('love')}>
           {love ? 'Yes' : 'No'}
         </button>
       </p>
+      <p><button onClick={handleLike}>Give Love</button> {likeCount}</p>
     </div>
   );
 };
